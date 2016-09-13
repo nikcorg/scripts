fb0cd04ada4fce83bc2f77489191f9e92ef0a33d
Niklas Lindgren <nikc@iki.fi>
2016-09-13 14:22:55 +0100

    feat: tab completion for npm scripts

---
+++ scripts/npm-run-completion.bash
@@ -0,0 +1,23 @@
+_find_all_scripts() {
+  if [[ -s "./package.json" ]]; then
+   cat package.json | jq -r '.scripts?|keys|.[]'
+  fi
+}
+
+_find_script() {
+  local cur prev opts
+  COMPREPLY=()
+
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+  if [[ "$prev" == "run" ]]; then
+    opts=$(_find_all_scripts)
+  fi
+
+  COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+
+  return 0
+}
+
+complete -o default -F _find_script npm
