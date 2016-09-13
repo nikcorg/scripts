e8b7efcc98054edc1a27941e6bce5a95f9c15c11
Niklas Lindgren <nikc@iki.fi>
2016-09-13 14:22:17 +0100

    style: whitespace

--- scripts/ssh-completion.sh
+++ scripts/ssh-completion.sh
@@ -1,18 +1,18 @@
 _find_all_ssh_hosts() {
-    grep "^Host " ~/.ssh/config | cut -d " " -f 2 | sort | uniq
+  grep "^Host " ~/.ssh/config | cut -d " " -f 2 | sort | uniq
 }
 
 _find_ssh_host() {
-    local cur prev opts
-    COMPREPLY=()
+  local cur prev opts
+  COMPREPLY=()
 
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts=$(_find_all_ssh_hosts)
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts=$(_find_all_ssh_hosts)
 
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+  COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
 
-    return 0
+  return 0
 }
 
 complete -o default -F _find_ssh_host ssh
